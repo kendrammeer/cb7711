b8d746fe771a068707cb04bea1077b26f3d074c1
kendrammeer <kendrammeer@gmail.com>
2018-09-06 12:42:43 -0600

    Add files via upload

--- hwk_1_7711.R
+++ hwk_1_7711.R
@@ -1,9 +1,19 @@
+#If ggplot2 package is not installed on your are (error with library(ggplot2) command), run comment below to install the package
+#install.packages("ggplot2")
+
+#Import ggplot2 from the package library
+library(ggplot2)
 
 #Initiate the values for the x-axis, True negative values 5->100
 TN <- seq(5,100,1)
 
 #Create a data frame from these x-values
 df <- data.frame(TN)
+
+#Add TP,FP,FN rows to the data frame with set values of 5
+df$TP <- rep(5,nrow(df))
+df$FP <- rep(5,nrow(df))
+df$FN <- rep(5,nrow(df))
 
 #Precision and recall functions
 precision <- function(TP,FP){
@@ -34,11 +44,12 @@ df$accuracy <- accuracy(df$TN,df$TP,df$FN,df$FP)
 
 #Plot F-score and accuracy
 p <- ggplot()+geom_point(data=df,aes(x=df$TN,y=df$fscore,shape="F-score"),size=3)+
-  +   geom_point(data=df,aes(x=df$TN,y=df$accuracy,shape="Accuracy"),size=3)+
-  +   scale_shape_manual(values=c(17,16))+theme_classic()
+  geom_point(data=df,aes(x=df$TN,y=df$accuracy,shape="Accuracy"),size=3)+
+  scale_shape_manual(values=c(17,16))+theme_classic()
 
-p_final<-p + labs(y='', x="True Negatives")+
-  +   theme(legend.title = element_blank())
+p_final<-p + labs(y='', x="True Negatives")+ theme(legend.title = element_blank())
+
+p_final
 
 #Save plot as png file
 png("Accuracy_Fscore_Plot.png")
